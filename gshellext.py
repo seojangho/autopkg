feeab44a7ba20d8df38560289ad35783ff803e02
JangHo Seo <jangho@jangho.io>
2017-12-02 18:28:43 +0900

    gshellext...

--- gshellext.py
+++ gshellext.py
@@ -4,6 +4,12 @@ import urllib
 import urllib.request as urlreq
 import json
 
+PACKAGE_NAME_FORMAT = 'gnome-shell-extension-{}'
+
+#class BuildPlan:
+#    @classmethod
+#    def for_extension(cls)
+
 class ExtensionInfo:
     cache_by_uuid = {}
     API_URL = 'https://extensions.gnome.org/extension-info/?uuid={}'
@@ -15,6 +21,15 @@ class ExtensionInfo:
         self.version = version
         self.download_url = download_url
         self.description = description
+
+    @classmethod
+    def from_uuid(cls, uuid):
+        if uuid in cls.cache_by_uuid:
+            return cls.cache_by_uuid[uuid]
+        else:
+            info = cls.from_uuid_uncached(uuid)
+            cls.cache_by_uuid[uuid] = info
+            return info
 
     @classmethod
     def from_uuid_uncached(cls, uuid):
