16bde27149c9f0459ca30c68fafccc23ccef490d
JangHo Seo <jangho@jangho.io>
2018-01-07 16:52:54 +0900

    gshellext backend done

--- src/backends.py
+++ src/backends.py
@@ -5,6 +5,8 @@ from utils import url_read
 from gzip import decompress
 from json import loads
 from package import PackageInfo
+from os.path import join
+from urllib.error import HTTPError
 
 
 class PkgbaseReference:
@@ -34,10 +36,21 @@ class PkgbaseReference:
         return self.pkgbase == other.pkgbase and self.backend == other.backend
 
 
-class AURBuildable:
-    def __init__(self, package_info):
+class AbstractBuildable:
+    def __init__(self, package_info, backend_name):
         self.package_info = package_info
-        self.pkgbase_reference = PkgbaseReference(self.package_info.pkgbase, 'aur')
+        self.pkgbase_reference = PkgbaseReference(self.package_info.pkgbase, backend_name)
+
+    def __str__(self):
+        return '{}→{}'.format(self.pkgbase_reference, self.package_info)
+
+    def __repr__(self):
+        return '\'{}->{}\''.format(self.pkgbase_reference, self.package_info)
+
+
+class AURBuildable(AbstractBuildable):
+    def __init__(self, package_info):
+        super().__init__(package_info, 'aur')
 
     def write_pkgbuild_to(self, path):
         """ :param path: Path to workspace.
@@ -51,12 +64,6 @@ class AURBuildable:
     def chroot_required(self):
         """ :return: True. """
         return True
-
-    def __str__(self):
-        return '{}→{}'.format(self.pkgbase_reference, self.package_info)
-
-    def __repr__(self):
-        return '\'{}->{}\''.format(self.pkgbase_reference, self.package_info)
 
 
 def extract_package_names(depends):
@@ -93,6 +100,7 @@ def aur_backend(pkgnames):
     return buildables
 
 
+GSHELLEXT_PKGREL = '-1'
 GSHELLEXT_PREFIX = 'gnome-shell-extension-'
 GSHELLEXT_PKGBUILD_FORMAT = """
 pkgname='{}'
@@ -100,15 +108,15 @@ pkgver={}
 pkgrel=1
 pkgdesc='{}'
 arch=('any')
-url='{}'
+url='https://extensions.gnome.org{}'
 license=('custom')
 depends=('gnome-shell')
-source=('{}')
+source=('https://extensions.gnome.org/download-extension/{}.shell-extension.zip?version_tag={}')
 sha256sums=('SKIP')
 
 package() {{
   extension_uuid='{}'
-  symlink_name='{}'
+  symlink_name='{}.shell-extension.zip?version_tag={}'
   rm -f "$symlink_name"
   install -d "${{pkgdir}}/usr/share/gnome-shell/extensions/${{extension_uuid}}"
   [[ -d schemas ]] && find schemas -name '*.xml' -exec install -Dm644 -t "$pkgdir/usr/share/glib-2.0/schemas/" '{{}}' +
@@ -120,29 +128,30 @@ package() {{
 """
 
 
-class GShellExtBuildable:
-    def __init__(self, package_info):
-        self.package_info = package_info
-        self.pkgbase_reference = PkgbaseReference(self.pkgbase, 'gshellext')
+class GShellExtBuildable(AbstractBuildable):
+    def __init__(self, package_info, uuid, version, version_tag, description, link):
+        super().__init__(package_info, 'gshellext')
+        self.uuid = uuid
+        self.version = version
+        self.version_tag = version_tag
+        self.description = description
+        self.link = link
 
     def write_pkgbuild_to(self, path):
         """ :param path: Path to workspace.
         :return: Path to the leaf directory where PKGBUILD resides.
         """
-        giturl = 'https://aur.archlinux.org/{}.git'.format(self.pkgbase)
-        run(['git', 'clone', giturl, path])
+        pkgbuild = GSHELLEXT_PKGBUILD_FORMAT.format(self.package_info.pkgname, self.version, self.description,
+                                                    self.link, self.uuid, self.version_tag, self.uuid, self.uuid,
+                                                    self.version_tag)
+        with open(join(path, 'PKGBUILD'), 'w') as f:
+            f.write(pkgbuild)
         return path
 
     @property
     def chroot_required(self):
-        """ :return: True. """
-        return True
-
-    def __str__(self):
-        return '{}→{}'.format(self.pkgbase_reference, self.package_info)
-
-    def __repr__(self):
-        return '\'{}->{}\''.format(self.pkgbase_reference, self.package_info)
+        """ :return: False. """
+        return False
 
 
 def gshellext_backend(pkgnames):
@@ -161,6 +170,19 @@ def gshellext_backend(pkgnames):
         if uuid in gshellext_backend.uuid_to_buildable:
             buildables.append(gshellext_backend.uuid_to_buildable[uuid])
             continue
+        try:
+            json = loads(url_read('https://extensions.gnome.org/extension-info/?uuid={}', uuid).decode())
+        except HTTPError:
+            continue
+        recent_version_pair = max(json['shell_version_map'].values(), key=lambda pair: pair['version'])
+        recent_version = recent_version_pair['version']
+        recent_version_tag = recent_version_pair['pk']
+        escaped_description = json['description'].replace('\'', '\\\'')
+        package_info = PackageInfo(GSHELLEXT_PREFIX + uuid.lower(), str(recent_version) + GSHELLEXT_PKGREL)
+        buildable = GShellExtBuildable(package_info, uuid, recent_version, recent_version_tag, escaped_description,
+                                       json['link'])
+        gshellext_backend.uuid_to_buildable[uuid] = buildable
+        buildables.append(buildable)
     return buildables
 
 
