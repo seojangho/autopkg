efdc26d012e884366c40cc19d3fbd4a968c9893e
JangHo Seo <jangho@jangho.io>
2018-01-07 21:29:06 +0900

    front

--- src/front.py
+++ src/front.py
@@ -11,28 +11,14 @@ from utils import log
 from utils import LogLevel
 from sys import argv
 from os import environ
-from archroot import arch_root
-from time import sleep
 
 BACKENDS = [git_backend, gshellext_backend, aur_backend]
 
 
-def test(pkgnames):
-    with run_lock():
-        repository = Repository('testrepo', '/home/jangho/workspace/testrepo')
-        graph = build_dependency_graph(pkgnames, BACKENDS)
-        plan = convert_graph_to_plan(graph, repository)
-        return plan
-
-
-def chroot():
-    with arch_root():
-        sleep(10)
-        pass
-
-
 if __name__ == '__main__':
-    log(LogLevel.debug, 'argv: {}', argv)
-    log(LogLevel.debug, 'AUTOPKG_HOME: {}', environ.get('AUTOPKG_HOME', None))
-    log(LogLevel.debug, 'AUTOPKG_KEY: {}', environ.get('AUTOPKG_KEY', None))
-    log(LogLevel.debug, 'AUTOPKG_RETRY: {}', environ.get('AUTOPKG_RETRY', None))
+    with run_lock():
+        log(LogLevel.debug, 'argv: {}', argv)
+        log(LogLevel.debug, 'AUTOPKG_HOME: {}', environ.get('AUTOPKG_HOME', None))
+        log(LogLevel.debug, 'AUTOPKG_KEY: {}', environ.get('AUTOPKG_KEY', None))
+        log(LogLevel.debug, 'AUTOPKG_RETRY: {}', environ.get('AUTOPKG_RETRY', None))
+        log(LogLevel.debug, 'Exiting...')
